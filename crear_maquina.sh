f63d1ecaecab3c6a3a6236a8670b76120156211c
Eduardo Palacios <empalacios@ing.usac.edu.gt>
2021-02-13 13:02:23 -0600

    Pass virtual machine parameters on script invokation.

--- crear_maquina.sh
+++ crear_maquina.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-vm_name=alpine2
-vm_directory=/home/user/qemu/$vm_name
-disk_name=$vm_directory/$vm_name.qcow2
-disk_size=16G
-ram_size=2048
-installation_image=/home/user/qemu/alpine/alpine-standard-3.8.0-x86_64.iso
-vnc_port=5900
+MINPARAMS=6
 
+if [ $# -lt "$MINPARAMS" ]
+then
+  echo "Usage $0 vm_name vms_directory disk_size ram_size install_image vnc_port"
+  echo "  vm_name:       Virtual machine's name"
+  echo "  vms_directory: Base directory where the virtual machine's directory will be created"
+  echo "  disk_size:     Capacity of the virtual machine's drive, in Gigabytes"
+  echo "  ram_size:      Capacity of the virtual machine's RAM, in Megabytes"
+  echo "  install_image: Path to the installation image (ISO)"
+  echo "  vnc_port:      VNC Port remote display clients"
+  exit 1
+fi
+
+
+vm_name=${1}
+qemu_vms_directory=${2}
+disk_size=${3}
+ram_size=${4}
+installation_image=${5}
+vnc_port=${6}
+vm_directory=$qemu_vms_directory/$vm_name
+disk_name=$vm_directory/$vm_name.qcow2
+
+
+if [ -e "$vm_directory" ]
+then
+  rm -rf $vm_directory
+fi
 mkdir $vm_directory
-qemu-img create -f qcow2 $disk_name $disk_size
-echo "Created hard drive $disk_name for $vm_name, size $disk_size"
+
+qemu-img create -f qcow2 $disk_name "${disk_size}G"
+echo "Created hard drive $disk_name for $vm_name, size $disk_size Gb"
 
 echo 'cdrom <installation_image> parameter is useful only for first'
 echo 'time start.'
